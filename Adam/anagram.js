18c054390bd2a86aade9c76bc391dcece5bca9a0
Adam Sakhr <adam.sakhr@gmail.com>
2016-06-01 17:32:27 +0200

    //test 5

--- Adam/anagram.js
+++ Adam/anagram.js
@@ -1,34 +1,41 @@
-(function() {
-  'use strict';
+//
+// This is only a SKELETON file for the "Anagram" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
 
-  function Anagram(word) {
+'use strict';
+
+function Anagram(word) {
     this.word = word;
-  }
+}
 
-  Anagram.prototype.match = function(words) {
+Anagram.prototype.matches = function(words) {
     var matches = [];
 
-    for(var i = 0; i < words.length; i++) {
-      var currentWord = words[i];
+    for (var i = 0; i < words.length; i++) {
+        var currentWord = words[i];
 
-      if (currentWord.length == this.word.length && currentWord != this.word) {
-        var currentWordLetters = currentWord.split('').sort();
-        var matchingWordLetters = this.word.split('').sort();
+        if (currentWord.length == this.word.length && currentWord != this.word) {
+            var currentWordLetters = currentWord.split('').sort();
+            var matchingWordLetters = this.word.split('').sort();
 
-        var isMatch = true;
+            var isMatch = true;
 
-        for (var j = 0; j < currentWordLetters.length; j++) {
-          if (currentWordLetters[j] != matchingWordLetters[j]) {
-            isMatch = false;
-          }
+            for (var j = 0; j < currentWordLetters.length; j++) {
+                if (currentWordLetters[j] != matchingWordLetters[j]) {
+                    isMatch = false;
+                }
+            }
+
+            if (isMatch) {
+                matches.push(currentWord);
+            }
         }
-
-        if (isMatch) { matches.push(currentWord); }
-      }
 
     }
     return matches;
-  };
+}
 
-  module.exports = Anagram;
-})();
+
+module.exports = Anagram;
